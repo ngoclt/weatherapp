fbb4a6968dbfb9be021755ff9eb43e8d430c6f9b
Ngoc Tuan Le <letuanngoc129@gmail.com>
2017-11-16 00:54:11 +0200

    Integrated API for History.

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { AppService } from '../app.service';
+
 @Component({
   selector: 'app-history',
+  providers: [AppService],
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  weathers: any;
+  errorMessage: string;
+
+  constructor(private service: AppService) { }
 
   ngOnInit() {
+    this.fetch();
   }
 
+  fetch() {
+    this.service.request('weather/last24hours', null)
+    .subscribe(
+      result => {
+        this.weathers = []
+        if (result.data != null) {
+          for (let record of result.data) {
+            let weather = {
+              date_time: new Date(record.date_time),
+              temperature: Math.ceil(record.temperature),
+              humidity: Math.ceil(record.humidity),
+              light: Math.ceil(record.light),
+              rain: Math.ceil(record.rain) === -100 ? 'NA' : Math.ceil(record.rain),
+            }
+            this.weathers.push(weather);
+          }
+        }
+      },
+      error => {
+        this.errorMessage = error;
+      }
+    );
+  }
 }
