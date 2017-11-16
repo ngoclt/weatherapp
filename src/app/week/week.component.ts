41709b9e568712e11c378a6138145367b20757e7
Ngoc Tuan Le <letuanngoc129@gmail.com>
2017-11-16 04:09:54 +0200

    Integrated API for Week page.

--- src/app/week/week.component.ts
+++ src/app/week/week.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { AppService } from '../app.service';
+
 @Component({
   selector: 'app-week',
+  providers: [AppService],
   templateUrl: './week.component.html',
   styleUrls: ['./week.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class WeekComponent implements OnInit {
 
-  constructor() { }
+  weathers: any;
+  errorMessage: string;
+
+  constructor(private service: AppService) { }
 
   ngOnInit() {
+    this.fetch();
   }
 
+  fetch() {
+    this.service.request('weather/last7days', null)
+    .subscribe(
+      result => {
+        this.weathers = []
+        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        if (result.data != null) {
+          for (let record of result.data) {
+            let date = new Date(record.date_time);
+            let dateString = date.toLocaleDateString("en-US", options);
+            let arr = dateString.split(",");
+            let weather = {
+              day: arr[0],
+              month: arr[1],
+              year: arr[2],
+              temperature: record.temperature,
+              humidity: record.humidity,
+            }
+            this.weathers.push(weather);
+          }
+        }
+      },
+      error => {
+        this.errorMessage = error;
+      }
+    );
+  }
 }
